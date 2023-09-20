15f0459b3fc1cfe70f1baa2f1207772d0ae5b8a1
juanandreacchio <juan.andreacchio@gmail.com>
2023-09-20 08:59:18 -0300

    solucion redirect

--- src/services/firebase/firebaseConfig.js
+++ src/services/firebase/firebaseConfig.js
@@ -2,8 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
 
-console.log(import.meta.env.VITE_apiKey);
-
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_apiKey,
     authDomain: import.meta.env.VITE_authDomain,
