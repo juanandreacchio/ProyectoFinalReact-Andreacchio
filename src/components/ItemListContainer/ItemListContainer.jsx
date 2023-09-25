88c24519618b6639062cd90734598c7a7a6672e3
juanandreacchio <juan.andreacchio@gmail.com>
2023-09-25 08:20:30 -0300

    sin productos en categoria

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { ItemList } from "../ItemList/ItemList";
 import classes from "./ItemListContainer.module.css";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useMode } from "../../context/ModeContext";
 import { SpinnerCircular } from "spinners-react";
 import { getProducts } from "../../services/firebase/firestore/products";
@@ -16,19 +16,18 @@ export const ItemListContainer = (props) => {
     setLoading(true);
 
     getProducts(categoryId)
-      .then(products =>{
-        setProductos(products)
-      } )
-      .catch(error => console.error(error))
-      .finally(() =>{
-        setLoading(false)
+      .then((products) => {
+        setProductos(products);
       })
-
+      .catch((error) => console.error(error))
+      .finally(() => {
+        setLoading(false);
+      });
   }, [categoryId]);
 
   const { mode } = useMode();
 
-  return (
+  return productos.length > 0 ? (
     <div
       className={
         (mode === "dark" && `${classes.itemListDm} ${classes.itemList}`) ||
@@ -38,6 +37,26 @@ export const ItemListContainer = (props) => {
       <h2 className={classes.greetingMsg}>{props.greeting}</h2>
       {!loading ? (
         <ItemList products={productos} />
+      ) : (
+        <SpinnerCircular
+          size="20%"
+          color="black"
+          style={{
+            width: "100%",
+            margin: "0 auto",
+            height: "200px",
+            marginTop: "50px",
+          }}
+        />
+      )}
+    </div>
+  ) : (
+    <div>
+      {!loading ? (
+        <div className={mode === 'light' ? `${classes.noCategoryContainer}` : `${classes.noCategoryContainer} ${classes.noCategoryContainerDM}`}>
+          <h1>Lo sentimos! No hay productos de esta categoría</h1>
+          <Link to="/">Volver al inicio</Link>
+        </div>
       ) : (
         <SpinnerCircular
           size="20%"
